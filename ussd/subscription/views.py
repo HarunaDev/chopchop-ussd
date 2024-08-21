f28e4ab2739a9e4dc0de61aaedf836b36af88371
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-21 06:47:54 +0100

    created route with POST method and saved parameters to get from the request inside of variables

--- ussd/subscription/views.py
+++ ussd/subscription/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
+
+# disable CSRF protection for this view to allow USSD requests without CSRF token verification
+@csrf_exempt
+def index(request):
+    # check if the request method is POST
+    if request.method == 'POST':
+        # get the neccesary parameters from the POST request
+        session_id = request.POST.get('sessionId')
+        service_code = request.POST.get('serviceCode')
+        phone_number = request.POST.get('phoneNumber')
+        text = request.POST.get('text', '')
